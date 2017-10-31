c1d685d3c89bee7a6e528cdba38b7c23f32d02af
msgctcheng <msgctcheng@gmail.com>
2017-10-31 14:29:18 -0400

    Basic Functionality, but ugly

---
+++ public/app.js
@@ -0,0 +1,79 @@
+
+$.getJSON("/articles", function(data) {
+
+  for (var i = 0; i < data.length; i++) {
+    $("#articles").append("<div class='newarticle'><p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><br />" + data[i].link + "<br /><br />"+ data[i].summary +"</p></div>");
+  }
+});
+
+
+
+$(document).on("click", "p", function() {
+
+  $("#notes").empty();
+
+  var thisId = $(this).attr("data-id");
+
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+
+    .done(function(data) {
+      console.log(data);
+
+      $("#notes").append("<h2>" + data.title + "</h2>");
+
+      $("#notes").append("<input id='titleinput' name='title' placeholder='Note Title'>");
+
+      $("#notes").append("<textarea id='bodyinput' name='body' placeholder ='Write a note!'></textarea>");
+     
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Note</button>");
+      if (data.note) {
+        $("#titleinput").val(data.note.title);
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+
+$(document).on("click", "#savenote", function() {
+
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  })
+    .done(function(data) {
+      console.log(data);
+      $("#notes").empty();
+    });
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+
+$(document).on("click", "#deletenote", function() {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: "",
+      body: ""
+    }
+  })
+    .done(function(data) {
+      console.log(data);
+      $("#notes").empty();
+    });
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
---
+++ server.js
@@ -0,0 +1,105 @@
+var cheerio = require("cheerio");
+var request = require("request");
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
+
+mongoose.Promise = Promise;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/scraping");
+var db = mongoose.connection;
+
+db.on("error", function (err) {
+    console.log("Mongoose Error", err);
+});
+db.once("open", function(){
+    console.log ("MongoDB Connection Sucessful!");
+});
+
+app.get("/scrape", function (req, res) {
+    request("https://www.nytimes.com/section/us?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=U.S.&WT.nav=page", function (err, resp, html) {
+        var $ = cheerio.load(html);
+
+
+
+        $("a.story-link").each(function (i, element) {
+            var result = {};
+            result.link = $(element).attr("href");
+
+            result.title = $(element).children("div.story-meta").children("h2.headline").text();
+
+            result.summary = $(element).children("div.story-meta").children("p.summary").text();
+
+            var entry = new Article(result);
+            entry.save(function(error, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(doc);
+                }
+            });
+        });
+    });
+    res.send("scrape complete");
+
+}); 
+
+app.get("/articles", function(req, res){
+    Article.find({}, function(err, doc) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(doc);
+        }
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+    Article.findOne({_id: req.params.id}).populate("note").exec(function (err, doc) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(doc);
+        }
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+    var newNote = Note(req.body);
+    newNote.save(function(err, doc){
+        if (err) {
+            res.send(err);
+            console.log("Error!");
+        } else {
+            console.log("No Error");
+            Article.findOneAndUpdate(
+                {_id: req.params.id},
+                {$set: {note: doc._id}},
+                {new: true},
+                function (error, newDoc) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log(newDoc);
+                        res.json(newDoc);
+                    }
+                }
+            )
+        }
+    });
+});
+
+app.listen(3000, function() {
+    console.log("App Running on Port 3000");
+});
